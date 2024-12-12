8f7347ab9c092870b84b63533480abcb3c1ce1b7
Jose Ignacio Gutierrez Guzman <jose.gutierrez@linktic.com>
2024-12-11 21:51:47 -0500

    TextField story

---
+++ packages/components/src/TextField/TextField.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Box, Text, Input } from "@chakra-ui/react";
+
+const TextField = ({ label, value, onChange, placeholder, errorMessage, isInvalid }) => {
+    return (
+        <Box>
+            {label && (
+                <Text mb={2} fontWeight="bold">
+                    {label}
+                </Text>
+            )}
+            <Input
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+                size="md"
+                borderColor={isInvalid ? "red.500" : "gray.300"}
+                aria-invalid={isInvalid}
+                _focus={{
+                    borderColor: isInvalid ? "red.500" : "blue.500",
+                }}
+            />
+            {isInvalid && errorMessage && (
+                <Text mt={1} fontSize="sm" color="red.500">
+                    {errorMessage}
+                </Text>
+            )}
+        </Box>
+    );
+};
+
+TextField.propTypes = {
+    label: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    placeholder: PropTypes.string,
+    errorMessage: PropTypes.string,
+    isInvalid: PropTypes.bool,
+};
+
+TextField.defaultProps = {
+    label: "",
+    value: "",
+    placeholder: "Enter text",
+    errorMessage: "",
+    isInvalid: false,
+};
+
+export default TextField;
---
+++ packages/components/src/TextField/TextField.stories.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import TextField from "./TextField";
+
+export default {
+    title: "Components/TextField",
+    component: TextField,
+};
+
+const Template = (args) => {
+    const [value, setValue] = useState("");
+
+    return (
+        <TextField
+            {...args}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+        />
+    );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+    label: "Default Label",
+    placeholder: "Enter text",
+};
+
+export const WithError = Template.bind({});
+WithError.args = {
+    label: "Error Label",
+    placeholder: "Enter text",
+    isInvalid: true,
+    errorMessage: "This field is required",
+};
